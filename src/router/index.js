7d68ced88bf484ddfbed7945b57ebcd709515e97
ouyangshunyu <ouyangshunyu@gmail.com>
2019-05-08 16:15:23 +0800

    更改路由

--- src/router/index.js
+++ src/router/index.js
@@ -22,6 +22,7 @@ import PageFive from '@/views/PageFive'
 import PageSix from '@/views/PageSix'
 import PageSeven from '@/views/PageSeven'
 import PageEight from '@/views/PageEight'
+import Mine from "../views/Mine";
 
 
 Vue.use(Router);
@@ -105,7 +106,46 @@ export default new Router({
                 }
 
             ]
+        },
+        {
+            path: '/mine',
+            name: 'Mine',
+            component: Mine
+        },
+        {
+            path: '/train',
+            name: 'Train',
+            component: Train,
+            redirect: '/recommend',
+            children: [
+                {
+                    path: '/recommend',
+                    name: 'Recommend',
+                    component: Recommend
+                },
+                {
+                    path: '/square',
+                    name: 'Square',
+                    component: Square
+                }
+            ]
+        },
+        {
+            path: '/vip',
+            name: 'Vip',
+            component: Vip
+        },
+        {
+            path: '/search',
+            name: 'Search',
+            component: Search
+        },
+        {
+            path: '/trends',
+            name: 'Trends',
+            component: Trends
         }
+
 
     ]
 })
